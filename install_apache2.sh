8cc803a2248fd6beb05b1d344a1ebd2a03ba6cc2
Oleksandr Ostron <aostron@gmail.com>
2024-05-07 19:50:28 +0200

    fix3

--- install_apache2.sh
+++ install_apache2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ -f /etc/debian_version ]; then
-echo "Installing on Debian(Ubuntu)"
-sudo apt install -y apache2
+  echo "Installing on Debian(Ubuntu)"
+  sudo apt install -y apache2
 elif [ -f /etc/redhat-release ]; then
-echo "Installing on Rhel(CentOS)"
-sudo yum install -y httpd
+  echo "Installing on Rhel(CentOS)"
+  sudo yum install -y httpd
 else
-echo "ERROR: Unknown system"
+  echo "ERROR: Unknown system"
 exit 1
 fi
